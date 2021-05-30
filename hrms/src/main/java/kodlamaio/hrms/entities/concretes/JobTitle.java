3d165dd3bcd403832e974404abb0e2ee21458d85
meyracan <canmeyra1@gmail.com>
2021-05-30 14:49:46 +0300

    HRMS-Day6Homework3
    Day 6 Homework 3 completed.

---
+++ hrms/src/main/java/kodlamaio/hrms/entities/concretes/JobTitle.java
@@ -0,0 +1,41 @@
+package kodlamaio.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="job_titles")
+public class JobTitle {
+	
+	@Id
+	@GeneratedValue
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="job_title")
+	private String jobTitle;
+
+	public JobTitle() {
+	}
+	public JobTitle(int id, String jobTitle) {
+		this.id = id;
+		this.jobTitle = jobTitle;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getJobTitle() {
+		return jobTitle;
+	}
+	public void setJobTitle(String jobTitle) {
+		this.jobTitle = jobTitle;
+	}
+	
+
+}
